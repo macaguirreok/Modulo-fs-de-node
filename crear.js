096b696cad7f770fe96790b3dc6ff35acf2027ea
macaguirreok <maquienyt@gmail.com>
2026-05-17 23:31:43 -0300

    f: crear completo

---
+++ crear.js
@@ -0,0 +1,26 @@
+import fs from "fs";
+
+const path = "alumnos.json";
+
+const data = fs.readFileSync( path, "utf-8");
+
+const alumnos = JSON.parse(data); //ya tengo los alumnos en array de objetos js
+
+//ahora creo el alumno que quiero agregar
+
+const alumno = { 
+    id: 1,
+    nombre: "Macarena",
+    edad: 28
+} 
+
+// Una vez creamos el alumno ---> esto después se lo hace desde una VISTA
+// lo agregamos al array de alumnos
+
+alumnos.push(alumno);
+
+//ahora guardamos esto que tenemos en js, en el archivo .json
+
+fs.writeFileSync ( path, JSON.stringify(alumnos, null, 2));
+
+console.log("Alumno añadido exitosamente");
